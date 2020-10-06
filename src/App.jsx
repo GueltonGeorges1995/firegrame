e1a1db21d91f383d01b16e492a3ad34266a8a844
Georges <georges.guelton@gmail.com>
2020-10-06 14:03:37 +0200

    ajout firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Title from './components/Title';
 import UploadForm from './components/UploadForm';
+import ImageGrid from './components/ImageGrid';
+import Modal from './components/Modal';
+
 const App = () => {
+
+    const [selectedImg, setSelectedImg] = useState('');
+
     return (
         <div className='app'>
             <Title />
             <UploadForm />
+            <ImageGrid setSelectedImg={setSelectedImg}/>
+            {
+                selectedImg !==''&&<Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg}/>
+            }
+            
         </div>
     )
 };
